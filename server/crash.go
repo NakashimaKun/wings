04c90f1bc34706b037cd931298f13fd2480c970d
NakashimaKun <bhillier90@gmail.com>
2025-03-10 10:16:51 +0000

    Update crash.go

--- server/crash.go
+++ server/crash.go
@@ -84,7 +84,7 @@ func (s *Server) handleServerCrash() error {
  		data += "\n" + line
  	}
 
-	data += "\n" + "Out of memory: " + oomKilled
+	data += "\n" + "Out of memory: %t", oomKilled
  
  	postErr := s.client.SendCrashLogs(s.Context(), s.Config().Uuid, data)
  
